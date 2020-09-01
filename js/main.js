0926192a6433ea27fc0c86f4689466e306199619
arjunvalipireddy <53044723+arjunvalipireddy@users.noreply.github.com>
2020-09-01 15:06:43 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Taarkik.", "DJ.", "Singer.", "Athlete.", "Artist"],
       typeSpeed: 100,
       loop: true,
     });
